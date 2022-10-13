247e5d3aa64b74297d6a7d14a0f8202b504388c2
minjin <kmj101012@naver.com>
2022-10-13 11:16:13 +0000

    model change 11:16:11 AM

--- test.sh
+++ test.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-id=minjiniese
-pw=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
 
-git add face_recogniser.pkl
-git commit -m "model change_test"
-git push -u origin master >> EOF
-${id}
-${pw}
-${id}
-${pw}
-EOF
+time=`date +%r`
+git commit -am "model change $time"
+git push -u origin master
 
 
 exit 0
-~
 
