20b8f70f5b048827a09059381196355274fe3cb9
minjin <kmj101012@naver.com>
2022-10-13 10:34:59 +0000

    test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+id=minjiniese
+pw=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
+
+git add face_recogniser.pkl
+git commit -m "model change_test"
+git push -u origin master >> EOF
+${id}
+${pw}
+${id}
+${pw}
+EOF
+
+
+exit 0
+~
+
